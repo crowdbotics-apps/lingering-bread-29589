aca7601a6b6beb89f57d06ce7061ff356cad27b6
Crowdbotics <team@crowdbotics.com>
2021-08-07 00:03:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "lingering_bread_29589Identifier",
+  url: "https://lingering_bread_29589Identifier.botics.co",
+  api: "https://lingering_bread_29589Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
